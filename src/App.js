7cb1febe6895b0b8c07ee751958dc105dd9ecf9b
Tony Lee <hy.tony.lee@gmail.com>
2019-10-29 20:33:34 -0700

    Add font awesome cdn

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import NavBar from './components/layout/NavBar';
 import './App.css';
 
 class App extends React.Component {
 	render() {
+		const name = 'John';
+		const loading = false;
+		const showName = true;
 		return (
 			<div className='App'>
 				<h1>GitHub Repo Finder</h1>
+				<NavBar />
+				{loading ? <h4>Loading... </h4> : <h4>Hello {showName && name}</h4>}
 			</div>
 		);
 	}
