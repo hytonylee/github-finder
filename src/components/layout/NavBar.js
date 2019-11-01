532ad8135d577d631e886d1f793d1d6048ed1a4f
Tony Lee <hy.tony.lee@gmail.com>
2019-11-01 11:42:07 -0700

    Add PropType in UserItem and User

--- src/components/layout/NavBar.js
+++ src/components/layout/NavBar.js
@@ -5,8 +5,7 @@ const NavBar = ({ icon, title }) => {
 	return (
 		<nav className='navbar bg-primary'>
 			<h1>
-				<i className={icon} />
-				{title}
+				<i className={icon} /> {title}
 			</h1>
 		</nav>
 	);
