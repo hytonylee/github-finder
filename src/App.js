1f8326a7fbc74cc3e7be2606d38732727b5f3918
Tony Lee <hy.tony.lee@gmail.com>
2019-11-01 22:51:12 -0700

    Add state in parent app level with axios

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
 import React from 'react';
 import NavBar from './components/layout/NavBar';
 import Users from './components/users/Users';
+import axios from 'axios';
 import './App.css';
 
 class App extends React.Component {
+	state = {
+		users: [],
+		loading: false
+	};
+
+	async componentDidMount() {
+		this.setState({ loading: true });
+		const res = await axios.get('https://api.github.com/users');
+		this.setState({
+			users: res.data,
+			loading: false
+		});
+	}
 	render() {
 		return (
 			<div className='App'>
 				<NavBar />
 				<div className='container'>
-					<Users />
+					<Users loading={this.state.loading} users={this.state.users} />
 				</div>
 			</div>
 		);
