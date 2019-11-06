e94051d0ec45564053877821ba3b7f73d143d65b
Tony Lee <hy.tony.lee@gmail.com>
2019-11-05 23:33:25 -0800

    Add Nav Link

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import NavBar from './components/layout/NavBar';
 import Search from './components/layout/Search';
 import Alert from './components/layout/Alert';
 import Users from './components/users/Users';
+import About from './components/pages/About';
 import axios from 'axios';
 import './App.css';
 
@@ -57,19 +59,32 @@ class App extends React.Component {
 		const { users, loading, alert } = this.state;
 
 		return (
-			<div className='App'>
-				<NavBar />
-				<div className='container'>
-					<Alert alert={alert} />
-					<Search
-						searchUsers={this.searchUsers}
-						clearUsers={this.clearUsers}
-						showClear={users.length > 0 ? true : false}
-						setAlert={this.setAlert}
-					/>
-					<Users loading={loading} users={users} />
+			<Router>
+				<div className='App'>
+					<NavBar />
+					<div className='container'>
+						<Alert alert={alert} />
+						<Switch>
+							<Route
+								exact
+								path='/'
+								render={props => (
+									<Fragment>
+										<Search
+											searchUsers={this.searchUsers}
+											clearUsers={this.clearUsers}
+											showClear={users.length > 0 ? true : false}
+											setAlert={this.setAlert}
+										/>
+										<Users loading={loading} users={users} />
+									</Fragment>
+								)}
+							/>
+							<Route exact path='/about' component={About} />
+						</Switch>
+					</div>
 				</div>
-			</div>
+			</Router>
 		);
 	}
 }
