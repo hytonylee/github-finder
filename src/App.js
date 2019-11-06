555ca79e2bfb7fa4a723a5fdff136659feaef327
Tony Lee <hy.tony.lee@gmail.com>
2019-11-05 16:09:24 -0800

    Add func protype to searchUsers

--- src/App.js
+++ src/App.js
@@ -11,19 +11,22 @@ class App extends React.Component {
 		loading: false
 	};
 
-	// async componentDidMount() {
-	// 	this.setState({ loading: true });
-	// 	const res = await axios.get(
-	// 		`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
-	// 	);
-	// 	this.setState({
-	// 		users: res.data,
-	// 		loading: false
-	// 	});
-	// }
+	async componentDidMount() {
+		this.setState({ loading: true });
+		const res = await axios.get(
+			`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+		);
+		this.setState({
+			users: res.data,
+			loading: false
+		});
+	}
 
 	// Search Github users
 	searchUsers = async text => {
+		this.setState({
+			loading: true
+		});
 		const res = await axios.get(
 			`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
 		);
